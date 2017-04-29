64b2eca1ea4942c20418343c47b15142f9773dae
iscream1 <iscream.94@gmail.com>
2017-04-29 23:06:14 +0200

    Revert "fully working editor"
    This reverts commit 50ca9f872e76b40d929e9311d77f588194ac8af2.

--- ShortcutDroid/ShortcutEditor.cs
+++ ShortcutDroid/ShortcutEditor.cs
@@ -28,7 +28,6 @@ namespace ShortcutDroid
             if(appList.Apps.Count!=0)
             {
                 int appIdx = (AppCombo.SelectedIndex == -1) ? 0 : AppCombo.SelectedIndex;
-                AppEditBox.Text = appList.Apps[appIdx].Name;
                 ProcessBox.Text = appList.Apps[appIdx].ProcessName;
                 ShortcutCombo.Text = "";
                 KeystrokeBox.Text = "";
@@ -40,10 +39,9 @@ namespace ShortcutDroid
         private void ShortcutCombo_SelectedIndexChanged(object sender, EventArgs e)
         {
             int appIdx = (AppCombo.SelectedIndex == -1) ? 0 : AppCombo.SelectedIndex;
-            if (appList.Apps.Count!=0&&appList.Apps[appIdx].ShortcutList.Count != 0)
+            if (appList.Apps[appIdx].ShortcutList.Count != 0)
             {
                 int shIdx = (ShortcutCombo.SelectedIndex == -1) ? 0 : ShortcutCombo.SelectedIndex;
-                ShortcutEditBox.Text = appList.Apps[appIdx].ShortcutList[shIdx].Label;
                 KeystrokeBox.Text = "";
                 KeystrokeBox.Text = appList.Apps[appIdx].ShortcutList[shIdx].Keystroke;
             }
@@ -53,21 +51,13 @@ namespace ShortcutDroid
         private void AddAppButton_Click(object sender, EventArgs e)
         {
             appList.Apps.Add(new App("My new application"));
-            AppEditBox.Text = "";
-            ProcessBox.Text = "";
-            ShortcutCombo.DataSource = null;
-            ShortcutEditBox.Text = "";
             AppCombo.SelectedIndex = AppCombo.Items.Count - 1;
         }
 
         private void RemoveAppButton_Click(object sender, EventArgs e)
         {
-            int appIdx = (AppCombo.SelectedIndex == -1) ? 0 : AppCombo.SelectedIndex;
-            if (appList.Apps.Count != 0)  appList.Apps.RemoveAt(appIdx);
-            AppEditBox.Text = "";
-            ProcessBox.Text = "";
-            ShortcutCombo.DataSource = null;
-            ShortcutEditBox.Text = "";
+            if (appList.Apps.Count != 0)  appList.Apps.RemoveAt(AppCombo.SelectedIndex);
+            ShortcutCombo.Text = "";
             KeystrokeBox.Text = "";
         }
 
@@ -78,39 +68,16 @@ namespace ShortcutDroid
 
             if (appList.Apps.Count!=0&&appList.Apps[shortcIdx].ShortcutList.Count!=0)
             {
-                appList.Apps[appIdx].ShortcutList[shortcIdx].Keystroke = KeystrokeBox.Text;
-                appList.Apps[appIdx].Name = AppEditBox.Text;
+                appList.Apps[appIdx].Name = AppCombo.Text;
                 appList.Apps[appIdx].ProcessName = ProcessBox.Text;
-                appList.Apps[appIdx].ShortcutList[shortcIdx].Label = ShortcutEditBox.Text;
+                appList.Apps[appIdx].ShortcutList[shortcIdx].Label = ShortcutCombo.Text;
+                appList.Apps[appIdx].ShortcutList[shortcIdx].Keystroke = KeystrokeBox.Text;
                 AppCombo.DataSource = null;
                 AppCombo.DataSource = appList.Apps;
                 ShortcutCombo.DataSource = null;
                 ShortcutCombo.DataSource = appList.Apps[appIdx].ShortcutList;
             }
             
-        }
-
-        private void AddShortcutButton_Click(object sender, EventArgs e)
-        {
-            if(appList.Apps.Count!=0)
-            {
-                int appIdx = (AppCombo.SelectedIndex == -1) ? 0 : AppCombo.SelectedIndex;
-                appList.Apps[appIdx].ShortcutList.Add(new Shortcut("My new CtrlS ExampleShortcut", "\\cs", false));
-                ShortcutEditBox.Text = appList.Apps[appIdx].ShortcutList[appList.Apps[appIdx].ShortcutList.Count - 1].Label;
-                KeystrokeBox.Text = appList.Apps[appIdx].ShortcutList[appList.Apps[appIdx].ShortcutList.Count - 1].Keystroke;
-                ShortcutCombo.SelectedIndex = ShortcutCombo.Items.Count - 1;
-            }
-            
-        }
-
-        private void RemoveShortcutButton_Click(object sender, EventArgs e)
-        {
-            int appIdx = (AppCombo.SelectedIndex == -1) ? 0 : AppCombo.SelectedIndex;
-            int shortcIdx = (ShortcutCombo.SelectedIndex == -1) ? 0 : ShortcutCombo.SelectedIndex;
-            ShortcutEditBox.Text = "";
-            KeystrokeBox.Text = "";
-            if (appList.Apps.Count!=0&&appList.Apps[appIdx].ShortcutList.Count != 0)
-                appList.Apps[appIdx].ShortcutList.RemoveAt(shortcIdx);
         }
     }
 }
